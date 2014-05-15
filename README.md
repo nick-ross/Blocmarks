38e06a25fa78f15b1bc0c38f51d79a794df776d6
Nick Ross <mx5433@gmail.com>
2014-05-15 09:33:15 -0500

    First commit and README update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blocmarks
+
+ Blocmarks is an application that allows users to bookmark URLs via email, peruse other user's bookmarks and maintain a personal index of categorized bookmarks.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocmarks::Application.config.secret_token = '013e43a739d44c4b3f98f708672d5c315f84d3b3b99e03b0bffee756d7d6240a7cb020d1a9b73c9120c5a84055b2bc11bc5b1cc57bd7ee804c28b4ce6a8170b4'
