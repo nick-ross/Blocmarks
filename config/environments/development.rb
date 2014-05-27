8c3457be020ca9a4a941105ce2058b4dafce8676
Nick Ross <mx5433@gmail.com>
2014-05-27 13:59:13 -0500

    Updated dev file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Blocmarks::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -38,12 +38,12 @@ Blocmarks::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :address        => 'smtp.smailgun.org',
+    :port           => 587,
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'app25329443.mailgun.org',
     :enable_starttls_auto => true
   }
 
